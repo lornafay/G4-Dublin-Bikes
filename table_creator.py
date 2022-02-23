c7ab5698b48b0a093df591677ea069d8e26ba632
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-02-23 13:55:19 +0000

    added backticks to query

--- table_creator.py
+++ table_creator.py
@@ -22,38 +22,39 @@ try:
 
     # prepare sql statement
     sql_static = f'CREATE TABLE IF NOT EXISTS static (' \
-                 f'index FLOAT,' \
-                 f'number FLOAT,' \
-                 f'name VARCHAR(50),' \
-                 f'address VARCHAR(50),' \
-                 f'latitude FLOAT,' \
-                 f'longitude FLOAT,' \
-                 f'bike_stands FLOAT);'
+                 f'`index` FLOAT,' \
+                 f'`number` FLOAT,' \
+                 f'`name` VARCHAR(50),' \
+                 f'`address` VARCHAR(50),' \
+                 f'`latitude` FLOAT,' \
+                 f'`longitude` FLOAT,' \
+                 f'`bike_stands` FLOAT);'
 
     sql_dynamic = f'CREATE TABLE IF NOT EXISTS dynamic (' \
-                  f'index FLOAT,' \
-                  f'number FLOAT,' \
-                  f'stands_available FLOAT,' \
-                  f'bikes_available FLOAT,' \
-                  f'status VARCHAR(50),' \
-                  f'time DATETIME);'
+                  f'`index` FLOAT,' \
+                  f'`number` FLOAT,' \
+                  f'`stands_available` FLOAT,' \
+                  f'`bikes_available` FLOAT,' \
+                  f'`status` VARCHAR(50),' \
+                  f'`time` DATETIME);'
+
     #possibly needs to be reviewed (weather API not working yet)
     sql_weather = f'CREATE TABLE IF NOT EXISTS weather (' \
-                  f'wind_speed_10m FLOAT,' \
-                  f'wind_dir_10m:d FLOAT,' \
-                  f'wind_gusts_10m_1h FLOAT,' \
-                  f'wind_gusts_10m_24h FLOAT,' \
-                  f't_2m FLOAT,' \
-                  f't_max_2m_24h FLOAT);'\
-                  f't_min_2m_24h FLOAT,' \
-                  f'msl_pressure FLOAT,' \
-                  f'precip_1h FLOAT,' \
-                  f'precip_24h:mm FLOAT,' \
-                  f'weather_symbol_1h VARCHAR(50),' \
-                  f'weather_symbol_24h VARCHAR(50),' \
-                  f'uv FLOAT,' \
-                  f'sunrise DATETIME),' \
-                  f'sunset DATETIME);'
+                  f'`wind_speed_10m` FLOAT,' \
+                  f'`wind_dir_10m:d` FLOAT,' \
+                  f'`wind_gusts_10m_1h` FLOAT,' \
+                  f'`wind_gusts_10m_24h` FLOAT,' \
+                  f'`t_2m` FLOAT,' \
+                  f'`t_max_2m_24h` FLOAT,'\
+                  f'`t_min_2m_24h` FLOAT,' \
+                  f'`msl_pressure` FLOAT,' \
+                  f'`precip_1h` FLOAT,' \
+                  f'`precip_24h:mm` FLOAT,' \
+                  f'`weather_symbol_1h` VARCHAR(50),' \
+                  f'`weather_symbol_24h` VARCHAR(50),' \
+                  f'`uv` FLOAT,' \
+                  f'`sunrise` DATETIME,' \
+                  f'`sunset` DATETIME);'
 
     # execute and apply sql command
     cursor.execute(sql_static)
