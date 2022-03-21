cad17c2317b269fa3c7854d004065c6b98742103
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-03-21 11:15:30 +0000

    routing between pages corrected

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,9 +3,16 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+# route for index page (Live Map)
 @app.route('/')
 def index():
     return render_template("index.html")
+
+
+# route when "Stations" seleceted from menu
+@app.route('/stations.html')
+def stations():
+    return render_template("stations.html")
 
 
 if __name__ == "__main__":
