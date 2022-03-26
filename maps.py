a5d51f3bd77b34524a57dece7732a0c4a6390d82
Colump <92551741+Colump@users.noreply.github.com>
2022-03-26 12:01:35 +0000

    new maps.py file

--- maps.py
+++ maps.py
@@ -1,33 +1,20 @@
-from flask import Flask, render_template, abort
+import mysql.connector
 
-app = Flask(__name__)
-
-class Station:
-    def __init__(self, key, name, lat, lng):
-        self.key  = key
-        self.name = name
-        self.lat  = lat
-        self.lng  = lng
-
-stations = (
-    Station('blessst', 'Blessington Street',     53.3568, -6.26814),
-   Station('boltonst', 'Bolton Streer',     53.3512, -6.26986),
-   Station('greekst', 'Greek Street',    53.3469, -6.27298)
+dataBase = mysql.connector.connect(
+    host="dublinbikes.c1gwxsai8wyx.eu-west-1.rds.amazonaws.com",
+    user="colum",
+    passwd="dubbikes22",
+    database="dublinbikes"
 )
 
-stations_by_key = {station.key: station for station in stations}
+cursorObject = dataBase.cursor()
 
-@app.route("/")
-def station():
-    return render_template('index.html', stations=stations)
+query = "SELECT number, address, bike_stands FROM dublinbikes.static;"
+cursorObject.execute(query)
 
-@app.route("/<station_code>") 
-def show_station(station_code):
-    station = stations_by_key.get(station_code)
-    if station:
-        #map file
-        return render_template('map.html', station=station)
-    else:
-        abort(404)
+myresult = cursorObject.fetchall()
 
-app.run(host='localhost', debug=True)
+for row in myresult:
+    print(row)
+
+dataBase.close()
