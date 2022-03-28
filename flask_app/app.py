0de67da52f2beb46ea89d0a9beca6ae3b8e5958f
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-03-28 11:38:44 +0100

    fixing env variables

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, render_template, request, jsonify, json
 from flask_mysqldb import MySQL
 from datetime import datetime, timedelta
+import os
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'dublinbikes.c1gwxsai8wyx.eu-west-1.rds.amazonaws.com'
-app.config['MYSQL_USER'] = 'colum'
-app.config['MYSQL_PASSWORD'] = 'dubbikes22'
+app.config['MYSQL_HOST'] = os.environ.get('RDS_HOST')
+app.config['MYSQL_USER'] = os.environ.get('RDS_USER')
+app.config['MYSQL_PASSWORD'] = os.environ.get('RDS_PASSWD')
 app.config['MYSQL_DB'] = 'dublinbikes'
 app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
 
 mysql = MySQL(app)
 
@@ -45,7 +47,7 @@ def get_stations():
 
 
 # route when "Weather" seleceted from menu
-@app.route('/weather.html')
+@app.route('/weather')
 def weather():
     return render_template("weather.html")
 
