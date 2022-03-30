8310ff144524dfc0de5a9d148eb32b580f858bde
Lorna Fay <lorna.fay@ucdconnect.ie>
2022-03-31 00:19:47 +0100

    stying completed

--- flask_app/static/js/dublinbikes.js
+++ flask_app/static/js/dublinbikes.js
@@ -14,55 +14,55 @@ var forecastBlock;
 function displayWeatherDublin(data){
 
     // html block for current weather
-    currentBlock =  `<div class="forecast_type"><button class="weather_button" id="current_button">Currently in Dublin</button><button class="weather_button" id="forecast_button" type="button" onclick="changeWeather();">Forecast</button></div>
-                            <div class="hours_forecast">
-                                <div class="small_current_box" id="current_icon">
-                                    <img src='` + iconURLPrefix + data.current.weather[0].icon + iconURLSuffix + `' alt='weather icon'>"
-                                </div>
-                                <div class="small_current_box" id="current_description">
-                                    <p><span id='current_time'>` + hours + ":" + minutes + "</span><br><br>"+ data.current.temp + `°C<br><br>
-                                    <span class='description'>` + data.current.weather[0].description + `</span><br><br>
-                                    rain: ` + data.minutely[0].precipitation + `mm</p>
-                                </div>
-                            </div>`;
+    currentBlock =  `<div class="forecast_type"><button class="weather_button" id="current_button" style="background-color:#aabdbd">Currently in Dublin</button><button class="weather_button" id="forecast_button" type="button" onclick="changeWeather();">Forecast</button></div>
+                    <div class="hours_forecast">
+                        <div class="small_forecast_box" id="current_icon_box">
+                            <img src='` + iconURLPrefix + data.current.weather[0].icon + iconURLSuffix + `' alt='weather icon'>
+                        </div>
+                        <div class="small_forecast_box">
+                            <p><span class="hr_font">` + hours + ":" + minutes + "</span><br><br>"+ data.current.temp + `°C<br><br>
+                            <span class='description'>` + data.current.weather[0].description + `</span><br><br>
+                            rain: ` + data.minutely[0].precipitation + `mm</p>
+                        </div>
+                    </div>`
 
     // html block for forecasted weather
-    forecastBlock = `<div class="forecast_type"><button class="weather_button" id="current_button" type="button" onclick="changeWeather();">Currently in Dublin</button><button class="weather_button" id="forecast_button">Forecast</button></div>
-                                <div class="hours_forecast" id="2-4hr_box">
-                                <div class= "small_forecast_box">
-                                    <p class="hr_font">2hr</p>
-                                    <p id="2hr">
-                                        <img src='` + iconURLPrefix + data.hourly[2].weather[0].icon + iconURLSuffix + `' alt='weather icon'>
-                                        <p><span class='description'>` + data.hourly[2].weather[0].description + `</span><br><br>
-                                        chance of rain: ` + Math.round(data.hourly[2].pop * 100) + `%</p>
-                                    </p>
-                                </div>
-                                <div class= "small_forecast_box">
-                                    <p class="hr_font">4hr</p>
-                                    <p id="4hr">
-                                        <img src='` + iconURLPrefix + data.hourly[4].weather[0].icon + iconURLSuffix + `' alt='weather icon'>
-                                        <p><span class='description'>` + data.hourly[4].weather[0].description + `</span><br><br>
-                                        chance of rain: ` + Math.round(data.hourly[4].pop * 100) + `%</p>
-                                    </p>
-                                </div>
-                                <div class= "small_forecast_box">
-                                    <p class="hr_font">6hr</p>
-                                    <p id="6hr">
-                                        <img src='` + iconURLPrefix + data.hourly[6].weather[0].icon + iconURLSuffix + `' alt='weather icon'>
-                                        <p><span class='description'>` + data.hourly[6].weather[0].description + `</span><br><br>
-                                        chance of rain: ` + Math.round(data.hourly[6].pop * 100) + `%</p>
-                                    </p>
-                                
-                                </div>
-                                <div class= "small_forecast_box">
-                                    <p class="hr_font">8hr</p>
-                                    <p id="8hr">
-                                        <img src='` + iconURLPrefix + data.hourly[8].weather[0].icon + iconURLSuffix + `' alt='weather icon'>
-                                        <p><span class='description'>` + data.hourly[8].weather[0].description + `</span><br><br>
-                                        chance of rain: ` + Math.round(data.hourly[8].pop * 100) + `%</p>
-                                    </p>
-                                </div>
-                            </div>`;
+    forecastBlock = `<div class="forecast_type"><button class="weather_button" id="current_button" type="button" onclick="changeWeather();">Currently in Dublin</button><button class="weather_button" id="forecast_button" style="background-color:#aabdbd">Forecast</button></div>
+                    <div class="hours_forecast">
+                    <div class= "small_forecast_box">
+                        <p class="hr_font">2hr</p>
+                            <p id="2hr">
+                            <img src='` + iconURLPrefix + data.hourly[2].weather[0].icon + iconURLSuffix + `' alt='weather icon'><br><br>
+                            <p><span class='description'>` + data.hourly[2].weather[0].description + `</span><br><br>
+                            chance of rain: ` + Math.round(data.hourly[2].pop * 100) + `%</p>
+                        </p>
+                    </div>
+                    <div class= "small_forecast_box">
+                        <p class="hr_font">4hr</p>
+                            <p id="4hr">
+                            <img src='` + iconURLPrefix + data.hourly[4].weather[0].icon + iconURLSuffix + `' alt='weather icon'><br><br>
+                            <p><span class='description'>` + data.hourly[4].weather[0].description + `</span><br><br>
+                            chance of rain: ` + Math.round(data.hourly[4].pop * 100) + `%</p>
+                        </p>
+                    </div>
+                    <div class= "small_forecast_box">
+                        <p class="hr_font">6hr</p>
+                            <p id="6hr">
+                            <img src='` + iconURLPrefix + data.hourly[6].weather[0].icon + iconURLSuffix + `' alt='weather icon'><br><br>
+                            <p><span class='description'>` + data.hourly[6].weather[0].description + `</span><br><br>
+                            chance of rain: ` + Math.round(data.hourly[6].pop * 100) + `%</p>
+                        </p>
+                    
+                    </div>
+                    <div class= "small_forecast_box">
+                        <p class="hr_font">8hr</p>
+                            <p id="8hr">
+                            <img src='` + iconURLPrefix + data.hourly[8].weather[0].icon + iconURLSuffix + `' alt='weather icon'><br><br>
+                            <p><span class='description'>` + data.hourly[8].weather[0].description + `</span><br><br>
+                            chance of rain: ` + Math.round(data.hourly[8].pop * 100) + `%</p>
+                        </p>
+                    </div>
+                </div>`;
 
         // page should load current weather by default
         document.getElementById("show_weather").innerHTML = currentBlock;
@@ -87,13 +87,22 @@ function changeWeather() {
     }
 }
 
-$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=53.350140&lon=-6.266155&units=metric&exclude=daily&appid={API_KEY}}", function(data){
+$.getJSON("https://api.openweathermap.org/data/2.5/onecall?lat=53.350140&lon=-6.266155&units=metric&exclude=daily&appid=C5826d96448b6ef028c87b0cf5da80b7", function(data){
 
     // set variables previously declared
     current_time = data.current.dt;
     time = new Date(current_time * 1000);
     hours = time.getHours();
+
+    // handle for time values less than 10
+    // otherwise the time 08:05 displays as 8:5
+    if (hours < 10){
+        hours = "0" + hours
+    }
     minutes = time.getMinutes();
+    if (minutes < 10){
+        minutes = "0" + minutes
+    }
     
     currentDisplayed = true;
 
